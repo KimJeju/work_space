18cf72c4f64dbac83b208f3a8176a5aa037de65e
kimjeju <h.hwi9367@gmail.com>
2024-02-08 17:49:48 +0900

    타스 블록체인

--- type_chain/src/index.ts
+++ type_chain/src/index.ts
@@ -1,16 +1,52 @@
-import {init} from "myPackage";
+import crypto from "crypto"
 
+interface IBlockShape{
+    hash:string;
+    prevHash:string;
+    height:number;
+    data:string;
+}
 
-init({
-    url:"true"
-})
+class Block implements IBlockShape {
 
-// class Block{
-//     constructor(
-//         private data:string
-//     ){}
-//     static hello(){
-//         return "hi"
-//     }
-// }
+    public hash:string
+    constructor(
+        public prevHash:string,
+        public height:number,
+        public data:string,
+    ){
+        this.hash = Block.calculateHash(prevHash,height,data);
+    }
 
+    static calculateHash(prevHash:string,height:number,data:string){
+        const toHash = `${prevHash}${height}${data}`; 
+        return crypto.createHash("sha256").update(toHash).digest("hex")
+    }
+
+}
+
+class BlockChain{
+    private blocks:Block[]
+    constructor(){
+        this.blocks = [];
+    }
+    private getPrevHash(){
+        if(this.blocks.length === 0) return ""
+        return this.blocks[this.blocks.length - 1].hash
+    }
+    public addBlock(data:string){
+        const newBlock = new Block(this.getPrevHash(),this.blocks.length + 1, data);
+        this.blocks.push(newBlock);
+    }
+    public getBloacks(){
+        return [...this.blocks];
+    }
+}
+
+const blockChain = new BlockChain();
+
+blockChain.addBlock("first");
+blockChain.addBlock("Second");
+blockChain.addBlock("somedata")
+
+console.log(blockChain.getBloacks());
--- type_chain/src/myPackage.js
+++ type_chain/src/myPackage.js
@@ -1,7 +1,22 @@
+
+//@ts-check
+/**
+ * 
+ * @param {object} config 
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns boolean
+ */
 export function init(config){
     return true;
 }
 
+//@ts-check
+/**
+ * 
+ * @param {number} code 
+ * @returns number
+ */
 export function exit(code){
     return code - 1
 }
