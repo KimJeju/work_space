cd18e0b86175e3f7387c7ba7563bf84ed557b8c9
kimjeju <h.hwi9367@gmail.com>
2024-02-14 07:19:19 +0900

    django Model Template 테스트

--- chat_bot/chatbot_server/fine_tuning_chatbot/views.py
+++ chat_bot/chatbot_server/fine_tuning_chatbot/views.py
@@ -1,7 +1,15 @@
+from django.views import View
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 # Create your views here.
 @api_view(['GET'])
 def hello_world(request):
     return Response("welcome django ")
+
+@api_view(['GET'])
+class HelloWorldView(View):
+    def get(self, request):
+        return Response("hello is cbv")
