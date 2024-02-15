bedfb8eb5e5b777305794aae6b3899a0386fe980
kimjeju <h.hwi9367@gmail.com>
2024-02-15 15:56:56 +0900

    django test api 생성

--- django_test/test_project/myapp/views.py
+++ django_test/test_project/myapp/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render,redirect
+from rest_framework import status
+
 from .forms import BookForm
 from myapp.models import Book
+from .serializsers import BookSerializer
+from  rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+# def create_book(request):
+#     if request.method == 'POST':
+#         form = BookForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('book_list')
+#     else:
+#         form = BookForm()
+#     return render(request, 'myapp/create_book.html', {'form' : form})
+
+@api_view(['POST'])
 def create_book(request):
-    if request.method == 'POST':
-        form = BookForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('book_list')
-    else:
-        form = BookForm()
-    return render(request, 'myapp/create_book.html', {'form' : form})
+    serializer = BookSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 def book_list(request):
     books = Book.objects.all()
--- django_test/test_project/test_project/settings.py
+++ django_test/test_project/test_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp'
+    'myapp',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,19 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES' : [
+        'rest_framework.permissions.IsAuthenticated'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES' : [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE':10,
+
+}
 
 ROOT_URLCONF = 'test_project.urls'
 
