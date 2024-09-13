4d7ef17ecd2284abebb01439d1ebfabab235c69e
kimjeju <h.hwi9367@gmail.com>
2024-09-13 15:30:54 +0900

    ㅎㅎ

--- learn_next/app/(movies)/movie/[id]/page.tsx
+++ learn_next/app/(movies)/movie/[id]/page.tsx
@@ -1,17 +1,22 @@
-import { API_URL } from "../../../(home)/page";
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
 
-async function getMovie(id:string) {
-    const response = await fetch(`${API_URL}/${id}`);
-    return response.json();
-}
-
-async function getVideos(id:string){
-    const response = await fetch(`${API_URL}/${id}/videos`);
-    return response.json();
-}
 
 export default async function MovieDetail({ params: { id } }: { params: { id: string } }) {
     //병렬처리
-    const [movie, videos] = await Promise.all([getMovie(id),getVideos(id)])
-    return <h1>{movie.title}</h1>
+    return (
+        <div>
+            {/* 
+            데이터 병렬처리를 위한 suspense 
+            fetch 가 동시에 진행 되지만 먼저 끝난 컴포넌트 부터 랜더링 해준다.
+            */}
+            <Suspense fallback={<h1>loading movie info</h1>}>
+                <MovieInfo id={id} />
+            </Suspense>
+            <Suspense fallback={<h1>loading movie video</h1>}>
+                <MovieVideos id={id} />
+            </Suspense>
+        </div>
+    )
 }
