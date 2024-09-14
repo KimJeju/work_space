d2d6225c27579d318875904f83f1223c6c38fc0f
kimjeju <h.hwi9367@gmail.com>
2024-09-14 18:27:37 +0900

    gdgd

--- hacking_source/app.py
+++ hacking_source/app.py
@@ -1,40 +1,19 @@
-#!/usr/bin/python3
-from flask import Flask, request, render_template, make_response, redirect, url_for
+#!/usr/bin/env python3
+from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
-try:
-    FLAG = open('./flag.txt', 'r').read()
-except:
-    FLAG = '[**FLAG**]'
-
-users = {
-    'guest': 'guest',
-    'admin': FLAG
-}
-
-@app.route('/')
+@app.route('/' , methods=['GET'])
 def index():
-    username = request.cookies.get('username', None)
-    if username:
-        return render_template('index.html', text=f'Hello {username}, {"flag is " + FLAG if username == "admin" else "you are not admin"}')
-    return render_template('index.html')
+    cmd = request.args.get('cmd', '')
+    if not cmd:
+        return "?cmd=[cmd]"
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
     if request.method == 'GET':
-        return render_template('login.html')
-    elif request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        try:
-            pw = users[username]
-        except:
-            return '<script>alert("not found user");history.go(-1);</script>'
-        if pw == password:
-            resp = make_response(redirect(url_for('index')) )
-            resp.set_cookie('username', username)
-            return resp 
-        return '<script>alert("wrong password");history.go(-1);</script>'
+        ''
+    else:
+        os.system(cmd)
+    return cmd
 
 app.run(host='0.0.0.0', port=8000)
